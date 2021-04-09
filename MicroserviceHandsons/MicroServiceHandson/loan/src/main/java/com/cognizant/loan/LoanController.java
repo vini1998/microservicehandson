bf662ec0120c18d2d40eac01a053eb35696748af
Vini Sasidharan <36506887+vini1998@users.noreply.github.com>
2021-04-09 15:27:59 +0530

    Add files via upload

---
+++ MicroserviceHandsons/MicroServiceHandson/loan/src/main/java/com/cognizant/loan/LoanController.java
@@ -0,0 +1,17 @@
+package com.cognizant.loan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+	@GetMapping("/{number}")
+	public Loan getLoanDetails(@PathVariable("number") String number) {
+		return new Loan(number, "car", 400000, 3258, 18);
+		
+	}
+}
---
+++ MicroserviceHandsons/MicroServiceHandson/zuul-gateway/src/main/java/com/cognizant/zuulgateway/SimpleFilter.java
@@ -0,0 +1,42 @@
+package com.cognizant.zuulgateway;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class SimpleFilter extends ZuulFilter {
+	private static Logger LOGGGER = LoggerFactory.getLogger(SimpleFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		RequestContext ctx=RequestContext.getCurrentContext();
+		HttpServletRequest request=ctx.getRequest();
+		LOGGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
